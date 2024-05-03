6be7aef8d222467adaa617d04528bb9fb081b33d
Mitsuha <miyammitsuha@gmail.com>
2024-05-03 16:06:19 +0800

    Fix build

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -1,7 +1,22 @@
 pub struct App {}
 
+struct AppList {}
+
+impl Iterator for AppList {
+    type Item = App;
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+impl AppList {
+    fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(AppList {})
+    }
+}
+
 impl App {
     pub fn list() -> Result<impl Iterator<Item = Self>, Box<dyn std::error::Error>> {
-        Vec::new()
+        Ok(AppList::new().ok().into_iter().flatten())
     }
 }
